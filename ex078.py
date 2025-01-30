bbcf55ebb7bbc700ac11aae5251147989d750bd2
gbrancaglion92 <ga.brancaglion@gmail.com>
2025-01-30 18:16:06 -0300

    exercicios Python

--- ex078.py
+++ ex078.py
@@ -1,20 +1,21 @@
 n = []
-for i in range (0,5):
-    n.append(int(input(f'Digite um valor para a posição {i}: ')))
-print('\033[33m/\033[m'*50)
-print(f'\033[33mA lista gerada foi {n}\033[m')
-print('\033[33m/\033[m'*50)
-
-n.sort()
-print(f'Lista organizada {n}')
+for c in range(0, 5):
+    n.append(int(input(f'Informe o {c + 1}º valor da posição {c}: ')))
 
 maior = max(n)
-posicao_maior = [i for i, v in enumerate(n) if v == maior]
-if len(posicao_maior) == 1:
-    print(f'O maior número digitado foi {max(n)} que está na posição {posicao_maior[0]}')
-else:
-    print(f'O maior número digitado foi {max(n)} que está nas posições {posicao_maior}')
-
 menor = min(n)
-posicao_menor = [i for i, v in enumerate(n) if v == menor]
-print(f'O menor número digitado foi {min(n)} que está nas posições {posicao_menor}')
+p_maior = [i for i, v in enumerate(n) if v == maior]
+p_menor = [i for i, v in enumerate(n) if v == menor]
+
+print('\033[33m/\033[m'*30)
+print(f'Você digitou os valores {n}')
+if len(p_maior) > 1:
+    print(f'O maior número o é {maior} que se encontra nas posições {p_maior}')
+else:
+    print(f'O maior número o é {maior} que se encontra na posição {p_maior}')
+
+if len(p_menor) == 1:
+    print(f'O menor número o é {menor} que se encontra na posição {p_menor}')
+else:
+    print(f'O menor número o é {menor} que se encontra nas posições {p_menor}')
+    
--- ex079.py
+++ ex079.py
@@ -1,13 +1,15 @@
 n = []
 while True:
-    valor = int(input('Informe um número: '))
-    if valor == -1:
-        print('Programa encerrado.')
-        break
-    if valor in n:
-        print(f'O número {valor} já foi inserido, por favor entre outro número.')
-    else:
-        n.append(valor)
-        print(f'O número {valor} foi adicionado a sua lista.')
-print(f'A lista gerada foi: \033[33m{n}')
+    n.append(int(input('Digite um valor: ')))
+    print('Valor adicionado com sucesso')
 
+    while True:
+        continuacao = str(input(f'Deseja continuar? [S/N]')).upper().strip()
+        if continuacao in ['S', 'N']:
+            break
+        print(f'Opção incorreta. Tente novamente.')
+    if continuacao == 'N':
+        print(f'Os valores inseridos foram: \033[33m{n}\033[m')
+        print(f'Programa encerrado')
+        break
+    
